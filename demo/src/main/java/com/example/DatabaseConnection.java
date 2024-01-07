5391a1dbebde2fedf1ca95400c053eebb3939a55
Pra_Kas <prakash63690@gmail.com>
2024-01-07 12:54:49 +0530

    MADE IT AS SINGLETON STRUCTURE

--- demo/src/main/java/com/example/DatabaseConnection.java
+++ demo/src/main/java/com/example/DatabaseConnection.java
@@ -8,25 +8,8 @@ public class DatabaseConnection {
     static final String PASS = "prakash";
     static String queString = "select * from student_details";
 
-    public static void main(String[] args) throws SQLException{
-            Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(queString);
-
-            // Print column headers
-            ResultSetMetaData metaData = rs.getMetaData();
-            int columnCount = metaData.getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-                System.out.print(metaData.getColumnName(i) + "\t");
-            }
-            System.out.println();
-
-            // Print records
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(rs.getString(i) + "\t");
-                }
-                System.out.println();
-            }
+    public static Connection databaseConnection() throws SQLException{
+        Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+        return con;
     }
-}
+};
